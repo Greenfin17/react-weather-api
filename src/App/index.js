69141eb6d992dd9985eb854ab0c3cd0ce54302b6
Greenfin17 <jmaple@yahoo.com>
2021-04-22 20:08:03 -0500

    adding minor styling

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import { React, useState } from 'react';
 import { getWeather } from '../helpers/data/weatherData';
-// import { getWeather } from '../helpers/data/weatherData';
 import './App.scss';
 
 function App() {
-  const [location, setLocation] = useState('Nashville');
+  const [location, setLocation] = useState('City');
   const [icon, setIcon] = useState('');
   const [temp, setTemp] = useState('');
   const [cond, setCond] = useState('');
@@ -12,27 +11,31 @@ function App() {
 
   const submitEvent = (e) => {
     e.preventDefault();
-    console.warn(e.target.form[0].value);
     const place = e.target.form[0].value;
     setLocation(place);
-    const city = location;
+    const city = place;
     const locationObj = {
       city,
     };
-    getWeather(locationObj).then((report) => {
-      setTemp(report.main.temp);
-      setCond(report.weather[0].main);
-      setIcon(report.weather[0].icon);
-      setName(report.weather.name);
-    });
+    if (place) {
+      getWeather(locationObj).then((report) => {
+        if (report) {
+          setTemp(report.main.temp);
+          setCond(report.weather[0].main);
+          setIcon(report.weather[0].icon);
+          setName(report.weather.name);
+        }
+      });
+    }
   };
 
   return (
     <div className='App'>
+      <h1>Weather</h1>
       <div className='weather-container'>
         <div className='card weather-card' style={{ width: '18rem' }}>
           <p>{location}</p>
-          <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} className='card-img-top' alt="current weather icon" />
+          { icon && <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} className='card-img-top' alt="current weather icon" /> }
           <div className="card-body">
             <h5 className="card-title">{name}</h5>
             <p className="card-text">{temp} | {cond}</p>
@@ -44,7 +47,7 @@ function App() {
           <div className='form-group'>
             <label htmlFor="inputCity">City</label>
             <input type="text" className='form-control' id="input-city"
-              required />
+              placeholder="Enter a City" required />
           </div>
           <button type="submit" className='btn btn-primary' id="btn-submit-location"
             onClick={submitEvent}>Get Current Conditions</button>
